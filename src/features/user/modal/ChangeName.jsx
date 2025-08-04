9a07405db7eb9cff156aea1508dc0cb02fa7246a
100ke <bhy9803@gmail.com>
2025-08-05 00:56:48 +0900

    [250804] 회원가입, 로그인 페이지 구현, 비밀번호 찾기 모달 진행중

---
+++ src/features/user/modal/ChangeName.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ChangeName() {
+  return (
+    <div className="change-name">
+      {/* 이름 입력란, 변경 버튼 */}
+      <dialog id="my_modal_3" className="modal"></dialog>
+    </div>
+  );
+}
+
+export default ChangeName;
