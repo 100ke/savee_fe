1cfcd98b09aac70765f51bbcabb1cd8e8dbe254b
mnxx2 <alsdk4931@gmail.com>
2025-08-06 02:56:56 +0900

    [250806] 트랜잭션 카드 구현중, 데이터 가져오기 (테스트 x), nodemon 설치, .env 파일 생성(토큰 저장), .gitignore에 .env 추가

--- src/features/ledgers/transaction/TransactionCard.jsx
+++ src/features/ledgers/transaction/TransactionCard.jsx
@@ -1,107 +1,48 @@
+import { useEffect, useState } from "react";
+
 function TransactionCard() {
+  const [error, setError] = useState(null);
+  const [ledgerId, setLedgerId] = useState(1);
+  const [transaction, setTransaction] = useState(null);
+  const token = process.env.REACT_APP_TOKEN;
+
+  const fetchTransactions = async () => {
+    try {
+      const ledId = Number(ledgerId);
+      const response = await fetch(`ledgers/${ledgerId}/transactions`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const transaction = await response.json();
+      console.log(transaction);
+      setTransaction(transaction);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+  }, [ledgerId]);
+
   return (
     <ul className="list bg-base-100 rounded-box shadow-md">
-      <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
-        Most played songs this week
+      <li className="p-4 pb-2 transacinfo text-xs opacity-60 tracking-wide">
+        {transaction.date}
       </li>
 
-      <li className="list-row">
-        <div className="text-4xl font-thin opacity-30 tabular-nums">01</div>
-        <div>
-          <img
-            className="size-10 rounded-box"
-            src="https://img.daisyui.com/images/profile/demo/1@94.webp"
-          />
+      <li className="list-row transac-card">
+        <div className="p-4 pb-2 transacinfo text-xs opacity-60 tracking-wide">
+          Most played songs this week
         </div>
+
         <div className="list-col-grow">
           <div>Dio Lupa</div>
           <div className="text-xs uppercase font-semibold opacity-60">
             Remaining Reason
           </div>
         </div>
-        <button className="btn btn-square btn-ghost">
-          <svg
-            className="size-[1.2em]"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <g
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2"
-              fill="none"
-              stroke="currentColor"
-            >
-              <path d="M6 3L20 12 6 21 6 3z"></path>
-            </g>
-          </svg>
-        </button>
-      </li>
-
-      <li className="list-row">
-        <div className="text-4xl font-thin opacity-30 tabular-nums">02</div>
-        <div>
-          <img
-            className="size-10 rounded-box"
-            src="https://img.daisyui.com/images/profile/demo/4@94.webp"
-          />
-        </div>
-        <div className="list-col-grow">
-          <div>Ellie Beilish</div>
-          <div className="text-xs uppercase font-semibold opacity-60">
-            Bears of a fever
-          </div>
-        </div>
-        <button className="btn btn-square btn-ghost">
-          <svg
-            className="size-[1.2em]"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <g
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2"
-              fill="none"
-              stroke="currentColor"
-            >
-              <path d="M6 3L20 12 6 21 6 3z"></path>
-            </g>
-          </svg>
-        </button>
-      </li>
-
-      <li className="list-row">
-        <div className="text-4xl font-thin opacity-30 tabular-nums">03</div>
-        <div>
-          <img
-            className="size-10 rounded-box"
-            src="https://img.daisyui.com/images/profile/demo/3@94.webp"
-          />
-        </div>
-        <div className="list-col-grow">
-          <div>Sabrino Gardener</div>
-          <div className="text-xs uppercase font-semibold opacity-60">
-            Cappuccino
-          </div>
-        </div>
-        <button className="btn btn-square btn-ghost">
-          <svg
-            className="size-[1.2em]"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <g
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2"
-              fill="none"
-              stroke="currentColor"
-            >
-              <path d="M6 3L20 12 6 21 6 3z"></path>
-            </g>
-          </svg>
-        </button>
       </li>
     </ul>
   );
