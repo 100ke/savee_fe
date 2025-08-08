379b975c16ca77fc6ac050974fc3022c162be3ff
100ke <bhy9803@gmail.com>
2025-08-08 10:27:46 +0900

    [250808] 라우터, 레이아웃 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout.jsx";
+import LedgerMain from "./pages/LedgerMain.jsx";
+import Support from "./pages/Support.jsx";
+import MyPage from "./pages/MyPage.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<Layout />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<Layout />}>
+        <Route path="ledger/*" element={<LedgerMain />} />
+        <Route path="support/*" element={<Support />} />
+        <Route path="mypage" element={<MyPage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,6 @@
 import NavBar from "./NavBar"; // 사이드바 컴포넌트
 
-import { BrowserRouter, Outlet } from "react-router-dom";
-import Support from "../pages/Support";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DailyLedger from "../features/ledgers/daily/DailyLedger.jsx";
-import WeeklyLedger from "../features/ledgers/weekly/WeeklyLedger.jsx";
-import LedgerMain from "../pages/LedgerMain.jsx";
-import MyPage from "../pages/MyPage.jsx";
-import Login from "../pages/Login.jsx";
-import Signup from "../pages/Signup.jsx";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
@@ -16,14 +8,8 @@ export default function Layout() {
       {/* 사이드바 */}
       <NavBar />
 
-        {/* 메인 콘텐츠 영역 */}
-        <main className="flex flex-1 bg-white overflow-visible justify-center pt-10 pb-10 flex-wrap">
-          <Routes>
-            <Route path="/ledger/*" element={<LedgerMain />} />
-            <Route path="/support/*" element={<Support />} />
-            <Route path="/mypage" element={<MyPage />} />
-          </Routes>
-
+      {/* 메인 콘텐츠 영역 */}
+      <main className="flex flex-1 bg-white overflow-visible justify-center pt-10 pb-10 flex-wrap">
         <Outlet />
       </main>
     </div>
