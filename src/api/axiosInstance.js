6e32101303996994aa3f9aed911384a748a161f4
kdavin <kdavin@naver.com>
2025-08-08 16:52:23 +0900

    [250808] 1.axios 인증 토큰 만료 코드 추가 2.공지사항 메인 ui 수정(수정 중) 3.페이지네이션 적용 중

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -22,4 +22,16 @@ instance.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+// 응답 인터셉터: 에러 처리나 공통 응답 처리
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.warn("토큰 만료 또는 인증 오류");
+      // 리프레시 토큰 처리나 로그아웃 처리 등을 여기에 추가 가능
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default instance;
