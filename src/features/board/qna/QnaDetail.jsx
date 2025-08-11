d653e3ee5781397361a0e89d5534c50a8e90fc36
kdavin <kdavin@naver.com>
2025-08-11 16:52:12 +0900

    [250811] qna 등록 추가 답변완료 수정중

--- src/features/board/qna/QnaDetail.jsx
+++ src/features/board/qna/QnaDetail.jsx
@@ -1,12 +1,13 @@
-export default function QnaDetail(content) {
-  const question = content.content;
+export default function QnaDetail({ question, answer }) {
+  const questionContent = question;
+  const answerContent = answer;
   return (
     <div className="flex gap-2 flex-col">
-      <div>{question}</div>
+      <div>{questionContent}</div>
       <hr className="border-[var(--black30)]" />
       <div className="flex">
         <span className="text-[var(--error-color)] text-lg mr-2">A</span>
-        <div className="mt-1">{question}</div>
+        <div className="mt-1">{answerContent}</div>
       </div>
     </div>
   );
---
+++ src/features/board/qna/QnaModal.jsx
@@ -0,0 +1,31 @@
+import QnaInput from "./QnaInput";
+export default function QnaModal() {
+  return (
+    <div>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="btn bg-[var(--accent-color)] text-white rounded-box"
+        onClick={() => document.getElementById("my_modal_2").showModal()}
+      >
+        문의 접수
+      </button>
+      <dialog id="my_modal_2" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">문의 작성하기</h3>
+          <QnaInput></QnaInput>
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+        </div>
+
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </div>
+  );
+}
