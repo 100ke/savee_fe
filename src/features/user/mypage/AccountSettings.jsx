9f32f53cd5b38a628eaf13e71df55b615bd27a3c
100ke <bhy9803@gmail.com>
2025-08-12 14:22:00 +0900

    [250812] 브라우저 title 변경, 회원 모달 리팩토링 - 이름/비밀번호 변경 동일값 에러처리 - 모달 open/close 상태값 처리 - 변경 완료 후 모달 닫기

--- src/features/user/mypage/AccountSettings.jsx
+++ src/features/user/mypage/AccountSettings.jsx
@@ -1,34 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import ChangeName from "../modal/ChangeName";
 import ChangePassword from "../modal/ChangePassword";
 
 function AccountSettings({ onInfoChange }) {
+  const [isNameModalOpen, setIsNameModalOpen] = useState(false);
+  const [isPwModalOpen, setIsPwModalOpen] = useState(false);
+
   return (
     <div className="account-settings mb-5">
       <div className="card bg-base-100 shadow-md border">
         <div className="card-body">
           <h2 className="card-title text-xl">계정 설정</h2>
           <div className="btn-group flex gap-30 text-lg">
-            <a
-              className="text-gray-400"
-              onClick={() =>
-                document.getElementById("change-name-modal").showModal()
-              }
+            <button
+              className="cursor-pointer text-gray-400"
+              onClick={() => setIsNameModalOpen(true)}
             >
               이름 변경
-            </a>
-            <a
-              className="text-gray-400"
-              onClick={() =>
-                document.getElementById("change-pw-modal").showModal()
-              }
+            </button>
+            <button
+              className="cursor-pointer text-gray-400"
+              onClick={() => setIsPwModalOpen(true)}
             >
               비밀번호 변경
-            </a>
+            </button>
           </div>
         </div>
-        <ChangeName onSuccess={onInfoChange} />
-        <ChangePassword onSuccess={onInfoChange} />
+        {isNameModalOpen && (
+          <ChangeName
+            isOpen={isNameModalOpen}
+            onSuccess={() => {
+              onInfoChange();
+              setIsNameModalOpen(false);
+            }}
+            onClose={() => setIsNameModalOpen(false)}
+          />
+        )}
+        {isPwModalOpen && (
+          <ChangePassword
+            isOpen={isPwModalOpen}
+            onSuccess={() => {
+              onInfoChange();
+              setIsPwModalOpen(false);
+            }}
+            onClose={() => setIsPwModalOpen(false)}
+          />
+        )}
       </div>
     </div>
   );
