aaaa3ca59a8a5205082343c6e139164840e45950
100ke <bhy9803@gmail.com>
2025-08-13 16:10:45 +0900

    테일윈드 설정에 html 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // CRA 프로젝트 내에서 Tailwind 적용할 파일 경로
+    "./public/index.html", // HTML 포함
   ],
   theme: {
     extend: {
       colors: {
         main: "#5cb0ff",
-        "main-lightest":"#c1e8ff",
-        "main-dark":"#5c92ef",
-        accent:"#422ef4",
-      }
+        "main-lightest": "#c1e8ff",
+        "main-dark": "#5c92ef",
+        accent: "#422ef4",
+      },
     },
   },
   plugins: [require("daisyui")], // DaisyUI 플러그인 등록
