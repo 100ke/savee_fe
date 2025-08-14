57151b7c7535d6dcf3e2d3cbf12620c1958b76f9
kdavin <kdavin@naver.com>
2025-08-14 16:52:47 +0900

    [250814] 메인 입력 모달 설정 중

--- src/features/main/MainHome.jsx
+++ src/features/main/MainHome.jsx
@@ -1,50 +1,30 @@
 import { useEffect, useState } from "react";
 import { getUserInfo } from "../user/userApi";
 import MainButton from "./Button";
-import MainStats from "./MainStats"
+import MainStats from "./MainStats";
 import MainAnalysis from "./MainAnalysis";
 import MainSurpports from "./MainSupport";
 import MainTransaction from "./MainTransaction";
+import { useNavigate } from "react-router-dom";
+
+import {
+  fetchCreateTransactions,
+  fetchGetLedgers,
+} from "../ledgers/TransactionApi";
 export default function Main() {
   const [userName, setUserName] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const btns = [
-    {
-      bgColor: "var(--accent-color)",
-      borderColor: "var(--accent-color)",
-      label: "지출 입력",
-      icon: "+",
-      color: "white",
-    },
-    {
-      bgColor: "var(--main-color)",
-      borderColor: "var(--main-color)",
-      label: "소비 입력",
-      icon: "+",
-      color: "white",
-    },
-    {
-      bgColor: "white",
-      borderColor: "var(--main-color)",
-      label: "내 가계부",
-      icon: ">",
-      color: "var(--main-color)",
-      amount: "1000",
-    },
-    {
-      bgColor: "white",
-      borderColor: "var(--accent-color)",
-      label: "공유 가계부",
-      icon: ">",
-      color: "var(--accent-color)",
-      amount: "1000",
-    },
-  ];
-  useEffect(() => {
-    const token = localStorage.getItem("accessToken");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalTabType, setModalTabType] = useState("expense");
 
+  const token = localStorage.getItem("accessToken");
+  const [ledgers, setLedgers] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
     const getUser = async () => {
       try {
         if (token) {
@@ -61,40 +41,108 @@ export default function Main() {
     };
     getUser();
   }, []);
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const data = await fetchGetLedgers(token);
+        setLedgers(data);
+      } catch (error) {
+        setError("내역을 저장하지 못했습니다.");
+      }
+    };
+
+    load();
+  }, []);
+
+  const handleAddTransactions = async () => {
+    // setModalTabType(tabType); // 버튼에서 tabType 전달
+    setModalOpen(true);
+  };
+
+  const btns = [
+    {
+      bgColor: "var(--accent-color)",
+      borderColor: "var(--accent-color)",
+      label: "수입 입력",
+      icon: "+",
+      color: "white",
+      handleClick: "handleAddTransactions",
+      tabType: "income",
+    },
+    {
+      bgColor: "var(--main-color)",
+      borderColor: "var(--main-color)",
+      label: "지출 입력",
+      icon: "+",
+      color: "white",
+      tabType: "expense",
+    },
+    {
+      bgColor: "white",
+      borderColor: "var(--main-color)",
+      label: "내 가계부",
+      icon: ">",
+      color: "var(--main-color)",
+      tabType: "",
+      amount: "1000",
+    },
+    {
+      bgColor: "white",
+      borderColor: "var(--accent-color)",
+      label: "공유 가계부",
+      icon: ">",
+      color: "var(--accent-color)",
+      tabType: "",
+      amount: "1000",
+    },
+  ];
   if (loading) return <p>로딩 중...</p>;
   if (error) return <p>에러: {error}</p>;
   return (
     <div className="flex flex-col justify-center gap-6 w-3/4">
       <div>
-      <h1>
-        {isLoggedIn ? (
-          <p>
-            <span className="text-[var(--accent-color)] font-bold">
-              {userName}
-            </span>{" "}
-            님의 가계부
-          </p>
-        ) : (
-          <button className="font-semibold cursor-pointer ml-2">환영합니다. Savee 입니다. 로그인 후 이용해주세요. <span className="text-[var(--main-color)] font-bold ml-1">&gt;</span></button>
-        )}
-      </h1>
-      <hr className="w-full" />
+        <h1>
+          {isLoggedIn ? (
+            <p>
+              <span className="text-[var(--accent-color)] font-bold">
+                {userName}
+              </span>{" "}
+              님의 가계부
+            </p>
+          ) : (
+            <button
+              className="font-semibold cursor-pointer ml-2 mb-2"
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              환영합니다. Savee 입니다. 로그인 후 이용해주세요.{" "}
+              <span className="text-[var(--main-color)] font-bold ml-1">
+                &gt;
+              </span>
+            </button>
+          )}
+        </h1>
+        <hr className="w-full" />
       </div>
       <div className="flex flex-row gap-5">
-      <div className="w-2/4 flex flex-wrap">
-        {btns.map((btn, idx) => (
-          <MainButton
-            key={idx}
-            bgColor={btn.bgColor}
-            borderColor={btn.borderColor}
-            label={btn.label}
-            icon={btn.icon}
-            color={btn.color}
-            amount={btn.amount}
-          ></MainButton>
-        ))}
-      </div>
-      <MainStats></MainStats>
+        <div className="w-2/4 flex flex-wrap">
+          {btns.map((btn, idx) => (
+            <MainButton
+              key={idx}
+              bgColor={btn.bgColor}
+              borderColor={btn.borderColor}
+              label={btn.label}
+              icon={btn.icon}
+              color={btn.color}
+              amount={btn.amount}
+              handleClick={handleAddTransactions}
+              tabType={modalTabType}
+              ledgers={ledgers}
+            ></MainButton>
+          ))}
+        </div>
+        <MainStats></MainStats>
       </div>
       <MainAnalysis></MainAnalysis>
       <div className="flex flex-wrap">
