2a83527f8800a3fd69f40a6016a9d99305163260
100ke <bhy9803@gmail.com>
2025-08-17 02:42:17 +0900

    [250817] 소비 분석 페이지 - 데이터 로드 간격 지정 (캐시에 저장하여 재호출 반복 방지) - 레이아웃 수정 - 코칭 전략 컴포넌트 UI 수정 (collapse, tooltip 적용)

--- src/features/analysis/Strategy.jsx
+++ src/features/analysis/Strategy.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Strategy({ data }) {
-  if (!data) {
+  if (
+    !data ||
+    !data.strategy ||
+    !data.strategy.tips ||
+    !data.strategy.cautions ||
+    !data.strategy.positiveHabits
+  ) {
     return (
       <div>
         <h3 className="text-2xl font-semibold">코칭 전략</h3>
@@ -11,30 +17,61 @@ function Strategy({ data }) {
   }
   return (
     <div className="strategy">
-      <h3 className="text-2xl font-semibold">코칭 전략</h3>
-      {/* 1. 절약할 수 있는 구체적인 전략 3가지
-      2. 주의가 필요한 소비 영역
-      3. 긍정적인 소비 습관 */}
-      <div className="tip">
+      <h3 className="text-2xl font-semibold mb-2">코칭 전략</h3>
+      <div className="tip mb-5">
         <div className="title flex gap-2 items-center">
           <h4 className="text-2xl font-bold accent">TIP</h4>
           <span>절약할 수 있는 구체적인 전략 3가지!</span>
         </div>
-        <p>1. {data.strategy.tips[0].item1}</p>
-        <p>2. {data.strategy.tips[1].item2}</p>
-        <p>3. {data.strategy.tips[2].item3}</p>
+        <div className="tip-list text-lg">
+          {data?.strategy?.tips?.map((tip, i) => (
+            <div
+              key={i}
+              className="collapse bg-base-100 border-base-300 border"
+            >
+              <input type="checkbox" />
+              <div className="collapse-title font-semibold">{tip.item}</div>
+              <div className="collapse-content text-base">
+                {tip.description}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="content text-xl card bg-base-100 shadow-md border p-5 flex flex-cols gap-2 mb-3">
-        <p>주의가 필요한 소비 영역</p>
-        <p>1. {data.strategy.cautions[0].item1}</p>
-        <p>2. {data.strategy.cautions[1].item2}</p>
-        <p>3. {data.strategy.cautions[2].item3}</p>
-      </div>
-      <div className="positive">
-        <p>긍정적인 소비 습관</p>
-        <p>1. {data.strategy.positiveHabits[0].item1}</p>
-        <p>2. {data.strategy.positiveHabits[1].item2}</p>
-        <p>3. {data.strategy.positiveHabits[2].item3}</p>
+      <hr className="mb-5" />
+      <div className="content flex flex-row xl:flex-col gap-2">
+        <div className="caution flex-1 card bg-base-100 shadow-md border p-5 flex flex-cols gap-2 mb-3">
+          <p className="text-xl font-semibold">
+            <span className="red">주의</span>가 필요한 소비 영역
+          </p>
+          {data?.strategy?.cautions?.map((caution, i) => (
+            <div
+              key={i}
+              className="tooltip tooltip-bottom"
+              data-tip={caution.description}
+            >
+              <p>
+                {i + 1}. {caution.item}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="positive flex-1 card bg-base-100 shadow-md border p-5 flex flex-cols gap-2 mb-3">
+          <p className="text-xl font-semibold">
+            <span className="blue">긍정</span>적인 소비 습관
+          </p>
+          {data?.strategy?.positiveHabits?.map((positiveHabit, i) => (
+            <div
+              key={i}
+              className="tooltip tooltip-bottom"
+              data-tip={positiveHabit.description}
+            >
+              <p>
+                {i + 1}. {positiveHabit.item}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/pages/Analysis.jsx
+++ src/pages/Analysis.jsx
@@ -8,29 +8,83 @@ function Analysis() {
   const [summary, setSummary] = useState(null);
   const [strategy, setStrategy] = useState(null);
 
-  useEffect(() => {
-    if (!summary) {
-      const fetchData = async () => {
-        try {
-          const summaryData = await getSummary();
-          setSummary(summaryData);
-          // 코칭 전략
-          const strategyData = await getStrategy(summaryData);
-          setStrategy(strategyData);
-        } catch (error) {
-          console.log("코칭 전략 로드 실패: ", error);
-        }
-      };
+  const TODAY = new Date().toISOString().split("T")[0];
 
-      fetchData();
+  // 로컬 스토리지에서 캐시 가져오기
+  const loadCachedData = () => {
+    const cachedSummary = localStorage.getItem("cachedSummary");
+    const cachedStrategy = localStorage.getItem("cachedStrategy");
+    const lastFetchDate = localStorage.getItem("lastFetchDate");
+
+    if (cachedSummary) setSummary(JSON.parse(cachedSummary));
+    if (cachedStrategy) setStrategy(JSON.parse(cachedStrategy));
+
+    return lastFetchDate;
+  };
+
+  // api 호출 및 캐시에 데이터 저장
+  const fetchAndCachedData = async () => {
+    try {
+      const summaryData = await getSummary();
+      const strategyData = await getStrategy(summaryData);
+      // 에러 체크
+      if (summaryData?.error || strategyData?.error) {
+        console.log("소비 분석 결과 호출 에러, 재시도 중...");
+        setTimeout(fetchAndCachedData, 1500);
+        return;
+      }
+
+      setSummary(summaryData);
+      setStrategy(strategyData);
+
+      localStorage.setItem("cachedSummary", JSON.stringify(summaryData));
+      localStorage.setItem("cachedStrategy", JSON.stringify(strategyData));
+      localStorage.setItem("lastFetchDate", TODAY);
+    } catch (error) {
+      console.log("코칭 전략 로드 실패: ", error);
     }
-  }, [summary]);
+  };
+
+  useEffect(() => {
+    const lastFetchDate = loadCachedData();
+
+    if (lastFetchDate !== TODAY || !summary || !strategy) {
+      fetchAndCachedData();
+    } else {
+      console.log("캐시 데이터 사용");
+    }
+  }, []);
+
+  // 기존) 페이지 로드마다 데이터 불러오는 방식
+  // useEffect(() => {
+  //   if (!summary) {
+  //     const fetchData = async () => {
+  //       try {
+  //         const summaryData = await getSummary();
+  //         setSummary(summaryData);
+  //         // 코칭 전략
+  //         const strategyData = await getStrategy(summaryData);
+  //         setStrategy(strategyData);
+  //       } catch (error) {
+  //         console.log("코칭 전략 로드 실패: ", error);
+  //       }
+  //     };
+
+  //     fetchData();
+  //   }
+  // }, [summary]);
 
   return (
     <div className="analysis w-3/4">
       <h1 className="text-3xl mb-5">소비분석</h1>
-      <Summary data={summary} />
-      <Strategy data={strategy} />
+      <div className="flex flex-col xl:flex-row gap-5 xl:gap-10">
+        <div className="flex-1">
+          <Summary data={summary} />
+        </div>
+        <div className="flex-1">
+          <Strategy data={strategy} />
+        </div>
+      </div>
     </div>
   );
 }
